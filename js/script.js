2b5e8ac0cf7777433f65b5686419276eab81864a
GiacomoArena <giacomoarena28@gmail.com>
2023-03-22 16:50:57 +0100

    update commit + start bonus

--- js/script.js
+++ js/script.js
@@ -2,19 +2,35 @@
 
 
 for(let i = 1; i <= 100; i++){
+  const section = document.querySelector("section")
   
+  const div = document.createElement('div');
+  div.classList.add('container')
+
+  const div2 = document.createElement('div');
+  div.classList.add('box')
+  
+  
+  section.append(div);
+  div.append(i);
+
   if(i % 3 === 0 && i % 5 === 0 ){
+    div.innerHTML = "FizzBuzz";
     console.log("FizzBuzz");
   }
   else if(i % 5 === 0 ){
+    div.innerHTML = "Buzz";
     console.log("Buzz");
   }
   else if(i % 3 === 0 ){
+    div.innerHTML = "Fizz";
     console.log('Fizz');
   }
   else {
     console.log(i);
   }
   
+
   
+  console.log(section);
 }
